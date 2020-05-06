34c701bae83885b37f1ea5182a01293009eba402
Jon Martini <39231017+jsmartini@users.noreply.github.com>
2020-05-06 14:01:31 -0500

    Update onehot.py

--- preprocessing/onehot.py
+++ preprocessing/onehot.py
@@ -2,14 +2,15 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 
-def onehot(df:pd.DataFrame, series_list:list):
     #creates a onehot encoding of each series in the series lists. creates dummy series, concats and drops the initial series from df
     #wrote this because pd.get_dummies would run out of memory in my jupyter instance, sklearn does it much more efficiently
+
+def onehot(df:pd.DataFrame, series_list:list):
     encoder = LabelBinarizer()
     transformed = []
     df.dropna()
     for series in series_list:
-        print("Starting {}".format(series))
+        #print("Starting {}".format(series))
         x = df[series]
         try:
             encoder = LabelBinarizer()
@@ -21,9 +22,8 @@ def onehot(df:pd.DataFrame, series_list:list):
             transformed.append(transform)
             df.drop(series, axis=1, inplace=True)
         except TypeError:
-            #the lazy way out
             df.drop(series, axis=1, inplace=True)
-            print("TypeError on Series, Dropping and Skipping")
+            print("TypeError on Series {0}, Dropping and Skipping".format(series))
     #print(transformed)
     transformed.append(df)
-    return pd.concat(transformed)
+    return pd.concat(transformed, axis=1)  
